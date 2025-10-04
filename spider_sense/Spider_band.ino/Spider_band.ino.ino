99963e442f8dd67b998c741d56fd9da28010d73f
asark1 <arshdeep276@gmail.com>
2025-10-04 16:03:16 -0500

    added Clamping to max changes

--- spider_sense/Spider_band.ino/Spider_band.ino.ino
+++ spider_sense/Spider_band.ino/Spider_band.ino.ino
@@ -6,15 +6,18 @@
 #define TRIG2 51
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
 
-#define MAX_CHANGE 200
+#define MAX_CHANGE 300
 
 SR04 sensor2 = SR04(ECHO2, TRIG2);
-long a;
-long b;
+
+
+long newDistance;
+long oldDistance;
 
 void setup() {
    Serial.begin(9600);
    delay(1000);
+  oldDistance = sr04.Distance();
 }
 
 
@@ -22,10 +25,16 @@ void setup() {
 void loop() {
 
 
-   a=sr04.Distance();
-   Serial.print(a);
-   Serial.println("cm");
-  delay(100);
+  newDistance=sr04.Distance();
+   
+  if(abs(newDistance - oldDistance) < MAX_CHANGE)
+  {
+    Serial.print(newDistance);
+    Serial.println("cm");
+    delay(100);
+  }
+   
+
   // b=sensor2.Distance();
   // Serial.print(b);
   // Serial.println("cm ---------------");
